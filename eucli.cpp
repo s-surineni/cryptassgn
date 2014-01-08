0b84033515dfdc25c3204206330a7009f6e06a15
sampath kumar <sampath.surineni@gmail.com>
2014-01-08 10:14:24 +0530

    gcd donegit push crypt master

--- eucli.cpp
+++ eucli.cpp
@@ -1,14 +1,17 @@
 #include<stdio.h>
 
 void checkBigSmall(int *,int *);//method to put bigger number in big and small in small
+int gcdFind(int b,int s);//method to find gcd using euclidean algorithm
 
 int main(){
-	int big,sml;
+	int big,sml,gcd;
 	printf("enter the numbers you want to find the gcd of \n");
 	scanf("%d %d",&big,&sml);
 	printf("numbers entered are \n %d \n %d\n",big,sml);
 	checkBigSmall(&big,&sml);
 	printf("numbers entered are \n %d \n %d\n",big,sml);
+	gcd=gcdFind(big,sml);
+	printf("GCD is %d \n ",gcd);
 	return 0;
 }
 
@@ -18,4 +21,16 @@ void checkBigSmall(int *b,int *s){
 	*s=(*b)-(*s);
 	*b=(*b)-(*s);
 }
+}
+
+
+int gcdFind(int b,int s){
+	int r=0;
+	if(s==0)
+		return b;
+		
+	r=b%s;
+	b=s;
+	s=r;
+	gcdFind(b,s);
 }
